f379ebe0d911b56495842d9681fae671ef7c5074
Bob <rwillford0719@gmail.com>
2022-05-22 12:32:11 -0500

    final commit

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -2,11 +2,13 @@ const router = require('express').Router();
 const { createNewNote } = require('../../lib/notes');
 const { notes } = require('../../db/db.json');
 
+//get all notes
 router.get('/notes', (req, res) => {
     let results = notes;
     res.json(results)
 })
 
+//adding to notes
 router.post('/notes', (req, res) => {
     req.body.id = notes.length.toString();
     let note = createNewNote(req.body, notes);
